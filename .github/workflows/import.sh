4f292680a46502e1b9a834ea25653df1c5c942af
ChaosRifle <DC3120@gmail.com>
2024-10-03 19:31:15 -0600

    patched various bugs, and renamed key in code

--- .github/workflows/import.sh
+++ .github/workflows/import.sh
@@ -30,12 +30,12 @@ echo ===========================================================================
 echo 'configuring github install..'
 #get github up and running
 mkdir --parents "$HOME/.ssh"
-DEPLOY_KEY_FILE="$HOME/.ssh/deploy_key"
-echo "${SSH_IMPORT_KEY}" > "$DEPLOY_KEY_FILE"
-chmod 600 "$DEPLOY_KEY_FILE"
+IMPORT_KEY_FILE="$HOME/.ssh/import_key"
+echo "${SSH_IMPORT_KEY}" > "$IMPORT_KEY_FILE"
+chmod 600 "$IMPORT_KEY_FILE"
 SSH_KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
 ssh-keyscan -H "$GIT_SERVER" > "$SSH_KNOWN_HOSTS_FILE"
-export GIT_SSH_COMMAND="ssh -i "$DEPLOY_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
+export GIT_SSH_COMMAND="ssh -i "$IMPORT_KEY_FILE" -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_FILE"
 
 
 
@@ -57,7 +57,7 @@ git clone --branch "dev" "$GIT_THIS_REPOSITORY" "$HOME/git/staging"
 
 rm -rf "$HOME/git/staging/$FILE"   #check if file from import repo matches $FILE before we remove it?
 
-cp "$HOME/git/import/$FILE" "$HOME/git/staging/$FILE"
+cp "$HOME/git/import/$FILE" "$HOME/git/staging/scripts/$FILE"
 
 
 #mv "$HOME/git/staging" "$OUTPUT_DIR"
